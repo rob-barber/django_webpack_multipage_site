5ce9211d77644b34efc2e8870b63c4387d229dac
Rob Barber <rob@mogabi.co>
2020-11-10 12:09:18 -0500

    Initial Polls Project complete

--- part-1/webpack_multipage/webpack_multipage/settings.py
+++ part-1/webpack_multipage/webpack_multipage/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
