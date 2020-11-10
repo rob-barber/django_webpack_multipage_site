c13560d000a59ec749d7c74e80547004488e3ab2
Rob Barber <rob@mogabi.co>
2020-11-10 12:35:38 -0500

    Part-2 - Added initial files

---
+++ part-2/start/webpack_multipage/webpack_multipage/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for webpack_multipage project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webpack_multipage.settings')
+
+application = get_wsgi_application()
