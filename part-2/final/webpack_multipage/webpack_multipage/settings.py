6441a52e80163bd382ed13c2210b91901aec2ace
Rob Barber <rob@mogabi.co>
2020-11-11 12:11:42 -0500

    Part-2 - Adjusted directory structure. - Updated Django settings for the new structure

--- part-2/final/webpack_multipage/webpack_multipage/settings.py
+++ part-2/final/webpack_multipage/webpack_multipage/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'webpack_multipage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    ('images', os.path.join(BASE_DIR, 'assets/images')),
+
+    ('js', os.path.join(BASE_DIR, 'web_dist/js')),
+    ('styles', os.path.join(BASE_DIR, 'web_dist/styles')),
+]
